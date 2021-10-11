4c925e5d612b9caf30f4368190379ebd5e8dd385
Elvin Camalzade <e.camalzade@itstep.edu.az>
2021-10-11 16:32:15 +0400

    Add project files.

---
+++ Project/DataAccess/SqlServer/BookRepository.cs
@@ -0,0 +1,47 @@
+﻿using Project.Domain.Abstraction;
+using Project.Domain.AdditionalClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DataAccess.SqlServer
+{
+    public class BookRepository : IBookRepository
+    {
+        public DataClassesDataContext DataContext { get; set; }
+        public BookRepository()
+        {
+            DataContext = new DataClassesDataContext();
+        }
+
+        public Book GetData(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public ObservableCollection<Book> GetAllData()
+        {
+            var books = from b in DataContext.Books
+                        select b;
+            return ObserverHelper.ToObservableCollection(books);
+        }
+
+        public void AddData(Book data)
+        {
+            DataContext.Books.InsertOnSubmit(data);
+            DataContext.SubmitChanges();
+        }
+
+        public void UpdateData(Book data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteData(Book data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project/Domain/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Project.Commands;
+using Project.DataAccess.SqlServer;
+using Project.Domain.Abstraction;
+using Project.Domain.AdditionalClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+
+        private readonly IRepository<Book> _repo;
+
+        public MainViewModel(IRepository<Book> repo)
+        {
+            _repo = repo;
+            AllBooks = _repo.GetAllData();
+            CurrentBook = new Book();
+
+            AddCommand = new RelayCommand((sender) =>
+              {
+                  //temp id
+                  CurrentBook.Id_Author = 1;
+                  CurrentBook.Id_Category = 1;
+                  CurrentBook.Id_Press = 1;
+                  CurrentBook.Id_Themes = 1;
+                  _repo.AddData(CurrentBook);
+                  AllBooks = _repo.GetAllData();
+              });
+
+        }
+
+        public RelayCommand AddCommand { get; set; }
+
+
+        private Book currentBook;
+
+        public Book CurrentBook
+        {
+            get { return currentBook; }
+            set { currentBook = value; OnPropertyChanged(); }
+        }
+
+
+        private ObservableCollection<Book> allBooks;
+        public ObservableCollection<Book> AllBooks
+        {
+            get { return allBooks; }
+            set { allBooks = value; OnPropertyChanged(); }
+        }
+
+    }
+}
